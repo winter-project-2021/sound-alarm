ee86a0f3f599a091b5dc266a9ada5cdc0fa80c4b
SonDongGyu <salvare00027@gmail.com>
2021-02-20 21:05:55 +0900

    update loading

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,16 +1,15 @@
-import { useCallback} from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import ReactLoading from 'react-loading';
 import '../style/App.scss';
 
 function Loading() {
 
     const { loading } = useSelector(state => state.loading);
-    const dispatch = useDispatch();
 
     return (
         <>
             {loading ? (<div className='Modal'>
-                        <div>로딩 중 입니다.</div>
+                        <div className='Loading'><ReactLoading type={'balls'} color={'#ffffff'} height={250} width={350} /></div>
                        </div>) : null}
         </>
     );
